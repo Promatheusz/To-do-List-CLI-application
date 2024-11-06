1a5b97c1ebef38786c305ec90098ab7b19fbd1c6
Promatheusz <mateuszszczerbiak230@gmail.com>
2024-11-06 11:10:26 +0100

    Add import, export functionality
    Created import_tasks() and export_tasks() functions to handle import and export from csv file. Also, I added function to merge the import from two lists of tasks.

--- file_handling.py
+++ file_handling.py
@@ -1,2 +1,40 @@
 # File to save, import, export csv files
 import csv
+
+# Csv file structure : ID(number), Task(string), Status(string), Date(date)
+# ID = unique number for tasks
+# Task = description of task to be done
+# Date_status = status of task by date (Pending, Due, Missed)
+# Task_status = status of task by user (Incomplete, Completed)
+# Date = deadline for task (empty if no deadline)
+
+# Function to import tasks from the csv file
+def import_tasks(file_name):
+    imported_tasks = []
+    try:
+        with open(file_name, 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+               imported_tasks.append(row)
+    except FileNotFoundError:
+        print("File not found.")
+    return imported_tasks
+
+# Function to export tasks to the csv file
+def export_tasks(exported_tasks):
+    try:
+        with open('export_tasks.csv', 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerows(exported_tasks)
+    except IOError as e:
+        print(f"Error writing to file: {e}")
+
+# Function to append task list with new tasks from the imported tasks
+def add_tasks_to_list(imported_tasks, task):
+    task_id = len(task) + 1
+    # Change the ID of the imported tasks
+    for i in range(len(imported_tasks)):
+        imported_tasks[i][0] = task_id
+        task_id += 1
+        task.append(imported_tasks[i])
+    return task
